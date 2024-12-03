5f5fe2d1a2d649d6a81da91477b6ccbef9ef5dd3
irushi716 <153503765+irushi716@users.noreply.github.com>
2024-12-03 14:39:12 +0530

    database-configuration

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
-
+// postgresql://postgres.pgjfeaiurktlfmvonkoc:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres
 return [
     'database' => [
-        'host' => 'localhost',
-        'port' => 3306,
-        'dbname' => 'myapp',
+        'host' => 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'port' => 6543,
+        'user' => 'postgres.pgjfeaiurktlfmvonkoc',
+        'password' => 'E33DBuX_DPCQe*y',
+        'dbname' => 'postgres',
         'charset' => 'utf8mb4'
     ],
 
