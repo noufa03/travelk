7654247f572540b257cc7e52a56870b83ca00d98
noufa03 <salim2002621@gmail.com>
2024-12-16 10:15:33 +0530

    traid=userid

--- Http/controllers/registration/allroles/store/store.php
+++ Http/controllers/registration/allroles/store/store.php
@@ -33,14 +33,20 @@ if ($user) {
     header('location: /');
     exit();
 } else {
-
-    $travelers=$db->query('INSERT INTO Travelers ( user_name, profile) VALUES (:user_name, :profile)',[
-        'user_name'=>$_POST['user_name'],
-        'profile'=>$_POST['profile']
-    ]);
+//TraID=UserID
+    $userid=$db->query("SELECT MAX(userID) AS maxuserID FROM users")->get() ;
+    $userid=$userid[0]['maxuserID']+1;
+   
     $user = $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
         'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+    
+    
+    $travelers=$db->query('INSERT INTO Travelers (TraID, user_name, profile) VALUES (:id,:user_name, :profile)',[
+        'id'=>$userid,
+        'user_name'=>$_POST['user_name'],
+        'profile'=>$_POST['profile']
     ]);
      
      
