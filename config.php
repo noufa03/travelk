b0373bb75da8d7141ba2fecca17c58a631fc1d74
noufa03 <salim2002621@gmail.com>
2025-01-22 11:44:51 +0530

    hehe

--- config.php
+++ config.php
@@ -1,25 +1,27 @@
 <?php
 
-return [
-    'database' => [
-        'host' => 'localhost',
+return 
+
+// [
+//     'database' => [
+//         'host' => 'localhost',
+//         'port' => 5432,
+//         'user' => 'postgres',
+//         'password' => 'newpassword',
+//         'dbname' => 'travelk',
+//         'charset' => 'utf8mb4',
+     
+//     ],
+    
+   [ 'database' => [
+        'host' => 'db.pgjfeaiurktlfmvonkoc.supabase.co',
         'port' => 5432,
         'user' => 'postgres',
-        'password' => 'newpassword',
-        'dbname' => 'travelk',
+        'password' => 'E33DBuX_DPCQe*y',
+        'dbname' => 'postgres',
         'charset' => 'utf8mb4',
-     
-    ],
-    
-    // 'database' => [
-    //     'host' => 'db.pgjfeaiurktlfmvonkoc.supabase.co',
-    //     'port' => 5432,
-    //     'user' => 'postgres',
-    //     'password' => 'E33DBuX_DPCQe*y',
-    //     'dbname' => 'postgres',
-    //     'charset' => 'utf8mb4',
         
-    // ]
+    ]
 
 
 ];
