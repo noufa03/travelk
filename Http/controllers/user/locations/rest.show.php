ba8c298879970f45339b438f8f991b740809c4a4
irushi716 <153503765+irushi716@users.noreply.github.com>
2025-04-11 17:16:00 +0530

    Added restaurant display details and enhanced styling for the location view, including a back button, photo gallery, and operating hours section.

--- Http/controllers/user/locations/rest.show.php
+++ Http/controllers/user/locations/rest.show.php
@@ -9,12 +9,17 @@ $id = $_GET['id'];
 // dd($id);
 
 $place = $db->query('SELECT * FROM locations WHERE "locationid" = :id', ['id' => $id])->find();
+// dd($place);
 $restid = $place['userid'];
 // dd($restid);
 $resturant_details = $db->query('SELECT * FROM restaurants WHERE "resID" = :resID', ['resID' => $restid])->find();
+$resturant_display_details = $db->query('SELECT * FROM restaurant_details WHERE "id" = :id', ['id' => $restid])->find();
+// dd($resturant_display_details);
 // dd($resturant_details);
+
 
 view('user/locations/rest.show.view.php', [
     'place' => $place,
-    'resturant_details' => $resturant_details
+    'resturant_details' => $resturant_details,
+    'resturant_display_details' => $resturant_display_details
 ]);
